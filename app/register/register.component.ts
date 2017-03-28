2e78c43a92e9b83c7a720168e2fff6306ff2b55c
Achal-S <achalshri93@gmail.com>
2017-03-28 20:05:13 +0530

    latest changes

--- app/register/register.component.ts
+++ app/register/register.component.ts
@@ -1,22 +1,32 @@
 ﻿import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { AlertService, UserService } from '../_services/index';
+import { AlertService, UserService,SharedService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'register.component.html'
+    templateUrl: 'register.component.html',
+    styleUrls: ['register.component.css']
 })
 
 export class RegisterComponent {
     model: any = {};
     loading = false;
-
+    isTermsConditionAccepted:boolean ;
+    isAccepted:boolean ;
     constructor(
         private router: Router,
         private userService: UserService,
-        private alertService: AlertService) { }
+        private alertService: AlertService
+       ) {
+           this.isTermsConditionAccepted= false;
+           this.isAccepted= false;
+        }
 
+
+enableNext(){
+   this.isTermsConditionAccepted= true;
+}
     register() {
         this.loading = true;
         this.userService.create(this.model)
